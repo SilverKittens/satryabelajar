6db30ee9f1ba71e49ebb60b40d3301d5755dbcd7
satryabgs <bagusatrya1901@gmail.com>
2018-03-07 21:38:50 +0700

    php
    email

--- form.php
+++ form.php
@@ -1,3 +1,25 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug =  4;
+$mail->SMTPAuth=true;
+$mail->Host='smtp.gmail.com';
+$mail->Port='587';
+$mail->SMTPSecure='tls';
+$mail->Username='noreplyteknologiinformasi@gmail.com';
+$mail->Password='12345678qwe';
+$mail->SetFrom('noreplyteknologiinformasi@gmail.com');
+$mail->Subject='Pesan Kesan';
+$mail->Body = $_POST["pesan_kesan"];
+$mail->AddAddress('bagusatrya1901@gmail.com');
+if($mail->send())
+echo 'sukses';
+else
+echo $mail->ErrorInfo;
+?>
 <html>
 <head>
 <style>
@@ -14,12 +36,10 @@
 </head>
 <body>
 
-<h1 style="margin: 20px">Terimakasih <?php echo $_POST["nama_depan_pengunjung"]; ?>, telah memberikan pesan kepada kami.</h1> <br>
+
+<h1 style="margin: 20px">Terimakasih, telah memberikan pesan kepada kami.</h1> <br>
 <br>
 <button type="button" style="left: 50%;position: absolute; border-radius: 5px; background-color: palegoldenrod; border: none"><a href="index.php" style="text-decoration: none;font-family:Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana,' sans-serif';font-size: 25px">Back</a></button>
-<?php
-	header('refresh:3; Location:index.php');
-	exit;
-	?>
+
 </body>
 </html>
