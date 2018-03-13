cfb0d8d4798b2524e6a1508eeeeda81af4ae11f8
satryabgs <bagusatrya1901@gmail.com>
2018-03-13 23:57:34 +0700

    tambahan

--- formkomentar.php
+++ formkomentar.php
@@ -14,12 +14,16 @@
 		$email=$_POST['email_komentar'];
 		$isi=$_POST['komentar'];
 		$sql = "INSERT INTO komentar (nama, email,time, isi) VALUES ('$nama', '$email', CURRENT_TIMESTAMP, '$isi')";
+		$query = mysqli_query($conn, $sql);
 
-		if ($conn->query($sql) === TRUE) {
-			echo "New record created successfully";
-		} else {
-			echo "Error: " . $sql . "<br>" . $conn->error;
-		}
+		// apakah query update berhasil?
+			if( $query ) {
+				// kalau berhasil alihkan ke halaman list-siswa.php
+				header('Location: index.php');
+			} else {
+				// kalau gagal tampilkan pesan
+				die("Failed to create");
+			}
 
 		$conn->close();
 ?>
--- update.php
+++ update.php
@@ -10,28 +10,17 @@
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		} 
-
-		// sql to delete a record
-		// cek apakah tombol daftar sudah diklik atau blum?
 		if($_POST["tombol"]=="Submit"){
-
-			// ambil data dari formulir
 			$nama=$_POST['nama_depan_komentar'];
 			$email=$_POST['email_komentar'];
 			$isi=$_POST['komentar'];
 			$id=$_POST['id'];
 			$sql = "UPDATE komentar SET `nama`='$nama', `email`='$email', `time`=CURRENT_TIMESTAMP, `isi`='$isi' WHERE `id`='$id'";
 
-			//var_dump($sql);exit;
 			$query = mysqli_query($conn, $sql);
-			//var_dump($query);exit;
-
-			// apakah query update berhasil?
 			if( $query ) {
-				// kalau berhasil alihkan ke halaman list-siswa.php
 				header('Location: index.php');
 			} else {
-				// kalau gagal tampilkan pesan
 				die("Failed to update");
 			}
 
